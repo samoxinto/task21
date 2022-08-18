aca0fd89f230b4b80c8c60c64109b7f72b9239de
Денис Степаненко <denis.stepanenko@hotmail.com>
2022-08-18 13:38:26 +0300

    Add project files.

---
+++ Task21/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x1:");
+            int x1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите y1:");
+            int y1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите z1:");
+            int z1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите x2:");
+            int x2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите y2:");
+            int y2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите z2:");
+            int z2 = int.Parse(Console.ReadLine());
+
+            double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+
+            Console.WriteLine(distance);
+
+            Console.ReadLine();
+        }
+    }
+}
